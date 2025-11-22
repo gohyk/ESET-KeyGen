c12542001b70e44a3a73cba6763a410e76ddebbb
gohyk <yongkiat92@gmail.com>
2025-11-22 23:19:59 +0800

    update subscriptions data-label

--- modules/EsetTools.py
+++ modules/EsetTools.py
@@ -131,10 +131,10 @@ class EsetKeygen(object):
 
         logging.info(f'[{self.mode}] Request sending...')
         console_log(f'\n[{self.mode}] Request sending...', INFO, silent_mode=SILENT_MODE)
-        self.driver.get('https://home.eset.com/subscriptions/choose-trial')
-        uCE(self.driver, f"return {GET_EBAV}('button', 'data-label', 'subscription-choose-trial-ehsp-card-button') != null")
+        self.driver.get('https://home.eset.com/onboarding/subscriptions')
+        uCE(self.driver, f"return {GET_EBAV}('button', 'data-label', 'onboarding-add-subscription-protect-card-trial') != null")
         if self.mode == 'ESET HOME':
-            uCE(self.driver, f"return {CLICK_WITH_BOOL}({GET_EBAV}('button', 'data-label', 'subscription-choose-trial-ehsp-card-button'))")
+            uCE(self.driver, f"return {CLICK_WITH_BOOL}({GET_EBAV}('button', 'data-label', 'onboarding-add-subscription-protect-card-trial'))")
         elif self.mode == 'SMALL BUSINESS':
             uCE(self.driver, f"return {CLICK_WITH_BOOL}({GET_EBAV}('button', 'data-label', 'subscription-choose-trial-esbs-card-button'))")
         try:
